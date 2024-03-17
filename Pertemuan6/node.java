b81008bf6ef87353fb4fbd1dc7b9685e9e93cf3f
pebryandrian <124904199+pebryandrian@users.noreply.github.com>
2024-03-17 19:02:33 +0700

    Add files via upload

---
+++ Pertemuan6/node.java
@@ -0,0 +1,25 @@
+package Pertemuan6;
+
+public class node {
+		 private double data;
+		    private node next;
+
+		    public node(double data) {
+		        this.data = data;
+		    }
+
+		    // Setter dan Getter
+		    public void setNext(node next) {
+		        this.next = next;
+		    }
+
+		    public double getdata() {
+		        return data;
+		    }
+
+		    public node getNext() {
+		        return next;
+		    }
+	}
+
+
